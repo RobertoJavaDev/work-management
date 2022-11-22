fe4cee4eed27fd3e3bd7a7a1d1da28bffea9dca5
RobertoJavaDev <robertojavadev@gmail.com>
2022-11-22 09:08:54 +0100

    initial commit

---
+++ src/main/java/pl/robertojavadev/workmanagementapp/WorkManagementAppApplication.java
@@ -0,0 +1,13 @@
+package pl.robertojavadev.workmanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/robertojavadev/workmanagementapp/WorkManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.robertojavadev.workmanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
