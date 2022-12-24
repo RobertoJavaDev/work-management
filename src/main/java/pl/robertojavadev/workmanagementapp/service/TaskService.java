09c439d5fe6cc256c900439f8eea1dc08d730535
RobertoJavaDev <robertojavadev@gmail.com>
2022-12-24 06:54:42 +0100

    implemented method to return single task in service

--- src/main/java/pl/robertojavadev/workmanagementapp/service/TaskService.java
+++ src/main/java/pl/robertojavadev/workmanagementapp/service/TaskService.java
@@ -1,10 +1,13 @@
 package pl.robertojavadev.workmanagementapp.service;
 
 import org.springframework.stereotype.Service;
+import pl.robertojavadev.workmanagementapp.dto.TaskDto;
 import pl.robertojavadev.workmanagementapp.model.Task;
 import pl.robertojavadev.workmanagementapp.repository.TaskRepository;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class TaskService {
@@ -18,5 +21,14 @@ public class TaskService {
     public List<Task> getAllTasks() {
 
         return taskRepository.findAll();
+    }
+
+    public TaskDto getTaskById(UUID id) {
+        Optional<Task> task = taskRepository.findById(id);
+        if (task.isPresent()) {
+            return task.get();
+        } else {
+            throw new ResourceNotFoundException("Task with that id doesn't exist");
+        }
     }
 }
