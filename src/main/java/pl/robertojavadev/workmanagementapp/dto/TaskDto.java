03d76f327df08dbe69afc1cbd53ce31b546815d8
RobertoJavaDev <robertojavadev@gmail.com>
2022-12-24 06:53:49 +0100

    added Id filed in TaskDto class

--- src/main/java/pl/robertojavadev/workmanagementapp/dto/TaskDto.java
+++ src/main/java/pl/robertojavadev/workmanagementapp/dto/TaskDto.java
@@ -4,9 +4,12 @@ import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import java.time.Instant;
+import java.util.UUID;
 
 @Data
 public class TaskDto {
+
+    private UUID id;
 
     @NotBlank(message = "Description must be not empty")
     private String description;
