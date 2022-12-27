ed3601f12fa5aea214a7b7bf5b39956ffa5a7062
RobertoJavaDev <robertojavadev@gmail.com>
2022-12-27 10:19:01 +0100

    implemented method argument not valid exception

--- src/main/java/pl/robertojavadev/workmanagementapp/exception/GlobalExceptionHandlerController.java
+++ src/main/java/pl/robertojavadev/workmanagementapp/exception/GlobalExceptionHandlerController.java
@@ -3,6 +3,7 @@ package pl.robertojavadev.workmanagementapp.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -13,8 +14,17 @@ public class GlobalExceptionHandlerController {
 
     @ExceptionHandler(ResourceNotFoundException.class)
     @ResponseStatus(HttpStatus.NOT_FOUND)
-    public String resourceNotFoundHandler(ResourceNotFoundException e){
-    log.error(String.format("ResourceNotFoundException: %s", e.getMessage()));
-    return e.getMessage();
+    public String resourceNotFoundHandler(ResourceNotFoundException e) {
+        log.error(String.format("ResourceNotFoundException: %s", e.getMessage()));
+        return e.getMessage();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public String handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        e.getBindingResult().getFieldErrors().forEach(m -> {
+            log.error(String.format("%s %s", m.getField(), m.getDefaultMessage()));
+        });
+        return e.getMessage();
     }
 }
