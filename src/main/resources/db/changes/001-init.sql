7c34af5bf06cb2b3a2ac2a1e3b9f241aea2fec2d
RobertoJavaDev <robertojavadev@gmail.com>
2022-12-28 08:28:31 +0100

    added new changeset in liquibase

--- src/main/resources/db/changes/001-init.sql
+++ src/main/resources/db/changes/001-init.sql
@@ -15,3 +15,34 @@ create table if not exists tasks
 insert into tasks (id, description, done) values
     (random_uuid(), 'Learn programming', false),
     (random_uuid(), 'Write application', false);
+
+--changeset RobertoJavaDev:001_3
+
+create table if not exists projects
+(
+    id              uuid not null primary key,
+    description     varchar(100) not null,
+    creation_date   datetime not null
+)
+
+--changeset RobertoJavaDev:001_4
+
+create table if not exists sections
+(
+    id          uuid not null primary key,
+    description varchar(100) not null
+)
+
+--changeset RobertoJavaDev:001_5
+
+alter table sections
+    add column project_id uuid null;
+alter table sections
+    add foreign key (project_id) references projects (id);
+
+--changeset RobertoJavaDev:001_6
+
+alter table tasks
+    add column task_section_id uuid null;
+alter table tasks
+    add foreign key (task_section_id) references sections (id);
