329a994684e5c8ae25f862d0fa537dfddf650e3a
RobertoJavaDev <robertojavadev@gmail.com>
2023-05-12 09:20:00 +0200

    refactor: Refactored service classes

--- src/main/java/pl/robertojavadev/workmanagementapp/service/ProjectService.java
+++ src/main/java/pl/robertojavadev/workmanagementapp/service/ProjectService.java
@@ -2,6 +2,7 @@ package pl.robertojavadev.workmanagementapp.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import pl.robertojavadev.workmanagementapp.dto.ProjectDto;
 import pl.robertojavadev.workmanagementapp.dto.ProjectMapper;
@@ -10,11 +11,9 @@ import pl.robertojavadev.workmanagementapp.exception.ResourceNotFoundException;
 import pl.robertojavadev.workmanagementapp.model.Project;
 import pl.robertojavadev.workmanagementapp.repository.ProjectRepository;
 
-import javax.transaction.Transactional;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -26,29 +25,36 @@ public class ProjectService {
 
     private final ProjectMapper projectMapper;
 
-    public List<Project> getAllProjects() {
-        return projectRepository.findAll();
+    @Transactional(readOnly = true)
+    public List<ProjectDto> getAllProjects() {
+        return projectRepository.findAll().stream()
+                .map(projectMapper::mapProjectEntityToProjectDto)
+                .toList();
     }
 
+    @Transactional
     public ProjectDto createProject(@Valid ProjectDto projectRequest) {
 
         Project project = new Project();
         project.setName(projectRequest.getName());
         project.setDescription(projectRequest.getDescription());
 
-        return projectMapper.mapProjectEntityToProjectDto(projectRepository.save(project));
+        return projectMapper.mapProjectEntityToProjectDto(project);
     }
 
-    public Optional<Project> getProjectById(UUID id) {
-
-        return projectRepository.findById(id);
+    @Transactional(readOnly = true)
+    public ProjectDto getProjectById(@NotNull UUID id) {
+        Project project = projectRepository.findById(id).orElseThrow(() -> {
+            throw new ResourceNotFoundException("Project with Id: " + id + " does not exist.");
+        });
+        return projectMapper.mapProjectEntityToProjectDto(project);
     }
 
     @Transactional
-    public ProjectDto updateProject(@NotNull UUID id, @NotNull @Valid ProjectDto projectRequest) {
+    public ProjectDto updateProject(@NotNull UUID id, @Valid ProjectDto projectRequest) {
 
         Project project = projectRepository.findById(id).orElseThrow(() -> {
-            throw new ResourceNotFoundException("Project does not exist, please change your request");
+            throw new ResourceNotFoundException("Project with Id: " + id + " does not exist.");
         });
 
         project.setName(projectRequest.getName());
@@ -57,12 +63,12 @@ public class ProjectService {
         return projectMapper.mapProjectEntityToProjectDto(project);
     }
 
-    public void deleteProject(final UUID id) throws ResourceNotDeletedException {
-
-        if (!projectRepository.existsById(id)) {
-            throw new ResourceNotDeletedException("Project does not exist, please change your request");
+    @Transactional
+    public void deleteProject(@NotNull UUID id) throws ResourceNotDeletedException {
+        if (projectRepository.existsById(id)) {
+            projectRepository.deleteById(id);
+        } else {
+            throw new ResourceNotDeletedException("Project with Id: " + id + " does not exist.");
         }
-
-        projectRepository.deleteById(id);
     }
 }
--- src/main/java/pl/robertojavadev/workmanagementapp/service/TaskService.java
+++ src/main/java/pl/robertojavadev/workmanagementapp/service/TaskService.java
@@ -2,6 +2,7 @@ package pl.robertojavadev.workmanagementapp.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
 import pl.robertojavadev.workmanagementapp.dto.TaskDto;
 import pl.robertojavadev.workmanagementapp.dto.TaskMapper;
@@ -10,8 +11,8 @@ import pl.robertojavadev.workmanagementapp.model.Task;
 import pl.robertojavadev.workmanagementapp.repository.TaskRepository;
 
 import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
 @Service
@@ -23,50 +24,54 @@ public class TaskService {
 
     private final TaskMapper taskMapper;
 
-    public List<Task> getAllTasks() {
-
-        return taskRepository.findAll();
+    @Transactional(readOnly = true)
+    public List<TaskDto> getAllTasks() {
+        return taskRepository.findAll().stream()
+                .map(taskMapper::mapTaskEntityToTaskDto)
+                .toList();
     }
 
-    public TaskDto getTaskById(UUID id) {
-        Optional<Task> task = taskRepository.findById(id);
-        if (task.isPresent()) {
-            return taskMapper.mapTaskEntityToTaskDto(task.get());
-        } else {
-            throw new ResourceNotFoundException("Task with that id doesn't exist");
-        }
+    @Transactional(readOnly = true)
+    public TaskDto getTaskById(@NotNull UUID id) {
+        Task task = taskRepository.findById(id).orElseThrow(() -> {
+            throw new ResourceNotFoundException("Task with Id: " + id + " does not exist.");
+        });
+        return taskMapper.mapTaskEntityToTaskDto(task);
     }
 
+    @Transactional
     public TaskDto createTask(@Valid TaskDto taskRequest) {
 
         Task task = new Task();
         task.setDescription(taskRequest.getDescription());
         task.setDeadline(taskRequest.getDeadline());
 
-        return taskMapper.mapTaskEntityToTaskDto(taskRepository.save(task));
+        return taskMapper.mapTaskEntityToTaskDto(task);
     }
 
-    public TaskDto updateTask(@Valid UUID id, TaskDto taskRequest) {
+    @Transactional
+    public TaskDto updateTask(@NotNull UUID id, @Valid TaskDto taskRequest) {
 
         Task task = taskRepository.findById(id).orElseThrow(() -> {
 
-            throw new ResourceNotFoundException("Task with that id doesn't exist");
+            throw new ResourceNotFoundException("Task with Id: " + id + " does not exist.");
         });
 
         task.setDescription(taskRequest.getDescription());
         task.setDeadline(taskRequest.getDeadline());
 
-        return taskMapper.mapTaskEntityToTaskDto(taskRepository.save(task));
+        return taskMapper.mapTaskEntityToTaskDto(task);
     }
 
-    public TaskDto toggleTask(@Valid UUID id) {
+    @Transactional
+    public TaskDto toggleTask(@NotNull UUID id) {
 
         Task task = taskRepository.findById(id).orElseThrow(() -> {
-            throw new ResourceNotFoundException("Task with that id doesn't exist");
+            throw new ResourceNotFoundException("Task with Id: " + id + " does not exist.");
         });
 
         task.setDone(!task.isDone());
 
-        return taskMapper.mapTaskEntityToTaskDto(taskRepository.save(task));
+        return taskMapper.mapTaskEntityToTaskDto(task);
     }
 }
