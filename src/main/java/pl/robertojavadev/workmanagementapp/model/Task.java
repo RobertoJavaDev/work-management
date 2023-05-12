72ad7ea571cb1004a024524ab41f12638e34f7d0
RobertoJavaDev <robertojavadev@gmail.com>
2023-05-12 08:46:36 +0200

    refactor: Refactored entity classes

--- src/main/java/pl/robertojavadev/workmanagementapp/model/Task.java
+++ src/main/java/pl/robertojavadev/workmanagementapp/model/Task.java
@@ -3,19 +3,22 @@ package pl.robertojavadev.workmanagementapp.model;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import java.time.Instant;
 import java.util.UUID;
 
 @Entity
-@Table(name = "tasks")
 @Getter
 @Setter
-@ToString
 @RequiredArgsConstructor
+@Table(name = "tasks")
 public class Task {
 
     @Id
@@ -34,11 +37,11 @@ public class Task {
     private Section section;
 
     public Task(String description, Instant deadline) {
-        this.id = UUID.randomUUID();
         this.description = description;
         this.done = false;
         this.deadline = deadline;
     }
+
     public Task(UUID id, String description, Instant deadline) {
         this.id = id;
         this.description = description;
