eefcfb1e99de67af4642f304585a0a2919141485
RobertoJavaDev <robertojavadev@gmail.com>
2023-05-12 08:55:17 +0200

    refactor: Refactored exception classes

--- src/main/java/pl/robertojavadev/workmanagementapp/exception/AppRuntimeException.java
+++ src/main/java/pl/robertojavadev/workmanagementapp/exception/AppRuntimeException.java
@@ -1,8 +1,8 @@
 package pl.robertojavadev.workmanagementapp.exception;
 
-public class AppRuntimeException  extends RuntimeException{
+public class AppRuntimeException extends RuntimeException {
 
-    public AppRuntimeException(String message){
+    public AppRuntimeException(String message) {
         super(message);
     }
 }
--- src/main/java/pl/robertojavadev/workmanagementapp/exception/ResourceNotFoundException.java
+++ src/main/java/pl/robertojavadev/workmanagementapp/exception/ResourceNotFoundException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Value not found")
-public class ResourceNotFoundException extends AppRuntimeException{
+public class ResourceNotFoundException extends AppRuntimeException {
 
     public ResourceNotFoundException(String message) {
         super(message);
